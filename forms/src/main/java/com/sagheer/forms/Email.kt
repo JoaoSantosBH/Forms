796072ab00a155e9b3f403bc90f36c038aab53e6
MrZardari <sagheerhzardari@gmail.com>
2019-11-14 22:43:35 +0500

    Initial commit

---
+++ forms/src/main/java/com/sagheer/forms/Email.kt
@@ -0,0 +1,10 @@
+package com.sagheer.forms
+
+import android.util.Patterns
+
+class Email {
+
+    fun isEmail(email: String): Boolean{
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches()
+    }
+}
